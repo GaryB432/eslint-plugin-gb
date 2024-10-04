f03187c411d0b8390dd6c04011509b9d1be9e91d
Gary Bortosky <garybort@gmail.com>
2024-10-04 15:42:18 -0500

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,15 @@
-export {};
+declare const flatBase: any;
+declare const flatRecommended: any;
+declare const pkg: any;
+declare const plugin: {
+  meta: {
+    name: string;
+    version: string;
+  };
+  configs: {
+    "flat/base": any;
+    "flat/recommended": any;
+  };
+  rules: {};
+  processors: {};
+};
